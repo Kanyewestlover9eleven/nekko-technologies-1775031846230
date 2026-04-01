82a68bb3194ad208ade30ac08be3e62856fba312
DatDawg <149214606+Kanyewestlover9eleven@users.noreply.github.com>
2026-04-01 16:24:13 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/nekko-technologies-1775031846230',
+  assetPrefix: '/nekko-technologies-1775031846230'
+};
+module.exports = nextConfig;
